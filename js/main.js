5141cd2f066aecab9f2de654e832bc92628b1b17
chesterxp <chesterxp7@wp.pl>
2017-06-30 12:18:49 +0200

    mechanizm dodawania danych w js

---
+++ js/main.js
@@ -0,0 +1,69 @@
+var songs = [
+  {title:"Dzięki Wam",text:"Jest na świecie tym, ktoś tak bardzo bliski,<br> Ktoś, kto w moim sercu zawsze miejsce ma,<br> Ktoś, kto mnie nauczył stawiać pierwsze kroki,<br> Podał mi swą dłoń i poprowadził w świat.<br> To dzięki wam, mam teraz to co mam,<br> To dzięki wam, wspanialszy jest mój świat,<br> Niech dobry Bóg was strzeże, niech wam da,<br> Siłę i zdrowie, byście mogli żyć sto lat....<br> Ref: Za Twój uśmiech mamo,<br> za Twe dłonie tato,<br> te które nieraz dały w kość,<br> Za to że jesteście,<br> zawsze blisko przy mnie, *2<br> Chcę dziś podziękować Wam.<br> To dzięki wam, mam teraz to co mam.......<br> <br>W życiu różnie bywa, są też przykre chwile.<br> A przekorny los nie zawsze sprzyja nam.<br> Wtedy zdajesz sprawę, ile trzeba siły,<br> Ile starań, by zbudować własny dom.<br> <br>To dzięki wam, mam teraz to co mam,<br> To dzieki wam, wspanialszy jest mój świat,<br> Niech dobry Bóg was strzeże, niech wam da,<br> Siłę i zdrowie, byście mogli żyć sto lat....<br> <br>Ref: Za Twój uśmiech mamo, za Twe dłonie tato. <br>"},
+  {title:"ALECHANDRO",text:"She's got both hands </br> In her pocket </br> And she won't look at you </br> Won't look you at </br> She hides through love </br> en su bolsillo </br> She got a halo around her finger </br> Around you </br> </br>You know that I love you boy </br> Hot like Mexico, rejoice </br> At this point I gotta choose </br> Nothing to loose </br> </br>Don't call my name </br> Don't call my name, Alejandro </br> I'm not your babe </br> I'm not your babe, Fernando </br> </br>Don't wanna kiss, don't wanna touch </br> Just smoke my cigarette and hush </br> Don't call my name </br> Don't call my name, Roberto </br> </br>Alejandro </br> Alejandro </br> Ale-ale-jandro </br> Ale-ale-jandro (2) </br> </br>Just stop </br> </br> Please, Just let me go </br> Alejandro, Just let me go </br> </br>She's not broken </br> She's just a baby </br> But her boyfriend's like a dad, just like a dad </br> Draw those flames that burn before him </br> Now he's gonna find a fight, gonnfool the bad </br> </br>You know that I love you boy </br> Hot like Mexico, rejoice </br> At this point I gotta choose </br> Nothing to loose </br> </br>Don't call my name </br> Don't call my name, Alejandro </br> I'm not your babe </br> I'm not your babe, Fernando </br> </br>Don't wanna kiss, don't wanna touch </br> Just smoke my cigarette and hush </br> Don't call my name </br> Don't call my name, Roberto </br> </br>Alejandro </br> Alejandro </br> Ale-ale-jandro </br> Ale-ale-jandro (2) </br>"},
+  {title:"Cry cry",text:"When she was a young girl  </br> She used to play with me  </br> I was her best friend  </br> We were inseparately  </br> We loved to ride our bikes  </br> Playin’ hide and seek  </br> Sneeking all the night  </br> Dancing in the street  </br> I look back at the time  </br> Now i realise  </br> She loved to play with fire  </br> I should have seen it in her eyes  </br> I should have seen it in her eDeep inside, you cry  </br> Don’t let your hope,die She fell in love for the fir </br> He was older t </br> Then he made her do </br> First she wouldn </br> She left everything </br> Couldn’t find  </br> Running through th </br> Loosing all he </br> She drugs away t </br> Turning off h </br> But still he makes  </br> Like everything’s  </br> Like everything’s Deep inside, you cry  </br> Don’t let your hope,die  </br>"},
+  {title:"Z  ciszą pośród 4 ścian",text:"To moja wina </br> Szukam odpowiednich słów </br> Nie mogłam cię zatrzymać </br> Może kiedyś wrócisz tu </br> Czas leczy rany </br> Ktoś otwiera cicho drzwi </br> Karmi złudzeniami </br> Tanią miłość daje mi </br> </br>Tak mijają dni bez ciebe </br> Z ciszą pośród czterech ścian </br> Nie przekrzyczy jej mój strach </br> Tak mijają dni bez ciebe </br> Każdy z nich jest taki sam </br> Tesknie już ostatni raz </br> </br>Czas dalej biegnie </br> A ja ciąglę w miejscu tkwię  </br> Chcę z nim biec do ciebie </br> Choć nie czekasz dobrze wiem </br> To moja wina </br> Wszystko mogło dłużej trwać </br> Mogłam cię zatrzymać </br> Staje z prawda twarzą w twarz </br> </br>Tak mijają dni bez ciebe </br> Z ciszą pośród czterech ścian </br> Nie przekrzyczy jej mój strach </br> Tak mijają dni bez ciebe </br> Każdy z nich jest taki sam </br> Tesknie już ostatni raz </br> </br>Może ciepło twoich ust </br> Może echo twoich słów  </br> Ukołyszą mnie do snu </br> Na poduszce zapach twój </br> Nim otulam się do snu </br> Chcę spokojnie zasnąć znów </br> </br>Tak mijają dni bez ciebe </br> Z ciszą pośród czterech ścian </br> Nie przekrzyczy jej mój strach </br> Tak mijają dni bez ciebe </br> Każdy z nich jest taki sam </br> Tesknie już ostatni raz </br>"},
+  {title:"aniaaaa",text:"To aniaaaa wam, <br> mam teraz to co mam,"},
+  {title:"basia",text:"To basia wam, <br> mam teraz to co mam,"},
+  {title:"Małgośka",text:"Jest na świecie tym, ktoś tak bardzo bliski,<br>Ktoś, kto w moim sercu zawsze miejsce ma,<br>Ktoś, kto mnie nauczył stawiać pierwsze kroki,<br>Podał mi swą dłoń i poprowadził w świat.<br>"},
+  {title:"krysia",text:"To krysiadzięki wam, <br> mam teraz to co mam,"},
+  {title:"zosia",text:"To zosia wam, <br> mam teraz to co mam,"},
+  {title:"Małgośka",text:"To dzięki wam, <br> mam teraz to co mam,"},
+  {title:"aniaaaa",text:"To aniaaaa wam, <br> mam teraz to co mam,"},
+  {title:"basia",text:"To basia wam, <br> mam teraz to co mam,"}
+];
+
+var x = [2,3,4];
+
+
+
+
+var main = $('#wrapper');
+var links = $('.boxOfTitle ul');
+
+
+for (var i=0;i<songs.length;i++){
+	var newLink = "";
+	var titleLink = songs[i].title;
+	newLink+= `<li><a href="#song${i}">${titleLink}</a></li>`;
+	links.append(newLink);
+
+	var newSong = "";
+	newSong += '<div class="title" id="song'+i+'">'+ songs[i].title+'</div>'+
+		'<div class="text">'+ songs[i].text+'</div>';
+	main.append(newSong);
+}
+
+
+console.log('dom');
+//console.log(x[1]);
+//console.log(songs[0].title);
+
+// $.ajax({
+// 	url:'dane2.txt',
+// 	success: function(dane){
+// 		//console.log( dane);
+//     var x = JSON.parse(dane);
+//     console.log(x.songs[0]);
+// 	},
+// 	error: function(){
+// 		console.log("błąd");
+// 	}
+// });
+
+
+var demo = $('#demo');
+var demo2 = $('#demo2');
+
+var ddd = '<p>'+songs[0].text +'</p>';
+demo.html(ddd);
+
+// demo2.load('dane2.txt',function(data){
+//   var x = JSON.parse(data);
+//   var y = x.songs[0].text;
+//   console.log(x.songs[0].text);
+//   demo.html(y);
+  
+// });
+
+//$('#demo2').html(ddd);
